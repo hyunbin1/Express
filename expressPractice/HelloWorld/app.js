84d7c9c91b984c673a0bd0cf414f258d5efab82d
hyunbin1 <kimgusqls1@gmail.com>
2022-03-12 15:04:35 +0900

    router 분리하기

--- expressPractice/HelloWorld/app.js
+++ expressPractice/HelloWorld/app.js
@@ -3,34 +3,26 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+// 정적인 파일 삽입을 위한 폴더 지정
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // jade 사용 선언
 app.locals.pretty = true;
 app.set("view engine", "jade");
 app.set("views", "./views");
-// 정적인 파일 삽입을 위한 폴더 지정
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// url = router, response = controller
-app.get("/", (req, res) => {
-  let title = ["Id 1 is!", "Id 2 is!", "Id 3 is!"];
-  let output = `
-  <a href="/title?id=0">query data 1</a></br>
-  <a href="/title?id=0">query data 2</a></br>
-  <a href="/title?id=0">query data 3</a></br>
-  ${title[req.query.id]}
-  `;
-  res.send(output);
+app.get("/topic/add", (req, res) => {
+  res.render("temp");
 });
 
-app.get("/image", (req, res) => {
-  res.send("<img src='/code.png'>");
-});
-
-app.get("/template", (req, res) => {
-  res.render("temp", { time: Date() });
+app.post("/topic", (req, res) => {
+  // let title = req.body.title;
+  // let description = req.body.description;
+  res.send("Hello post, " + req.body.title);
 });
 
 app.listen(port, () => {
-  console.log(`Connecting http://localhost:${port}`);
+  console.log(`Success to connect http://localhost:${port}`);
 });
---
+++ expressPractice/HelloWorld/tutorial.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3003;
+
+// jade 사용 선언
+app.locals.pretty = true;
+app.set("view engine", "jade");
+app.set("views", "./views");
+// 정적인 파일 삽입을 위한 폴더 지정
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// url = router, response = controller
+app.get("/", (req, res) => {
+  let title = ["Id 1 is!", "Id 2 is!", "Id 3 is!"];
+  let output = `
+  <a href="/title?id=0">query data 1</a></br>
+  <a href="/title?id=0">query data 2</a></br>
+  <a href="/title?id=0">query data 3</a></br>
+  ${title[req.query.id]}
+  `;
+  res.send(output);
+});
+
+app.get("/image", (req, res) => {
+  res.send("<img src='/code.png'>");
+});
+
+app.get("/template", (req, res) => {
+  res.render("temp", { time: Date() });
+});
+
+app.listen(port, () => {
+  console.log(`Connecting http://localhost:${port}`);
+});
