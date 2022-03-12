b242ccfd2f57f20d10c176e3e878b1f1c3346b05
hyunbin1 <kimgusqls1@gmail.com>
2022-03-13 00:43:29 +0900

    typeORM 사용을 위해 파일 삭제

--- mvcPractice/routes/board.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-// const controller = require("./controller");
-
-router.get("/", (req, res) => {
-  res.send("Get Method was successfully worked");
-});
-
-module.exports = router;
--- mvcPractice/routes/controller.js
+++
@@ -1,3 +0,0 @@
-exports.mainView = function (req, res) {
-  res.end("Hello");
-};
